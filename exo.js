e72cd6865cdaff53137f453651823231b1f0f47a
HizemMontassar <montassar.hizem@esprit.tn>
2023-02-28 23:48:55 +0100

    updating getColoredMap function

--- exo.js
+++ exo.js
@@ -54,18 +54,56 @@ class Map {
   }
 
   getColoredMap() {
+    var islands = this.getIslands();
+
     var coloredMap = [];
     for (var i = 0; i < this.map.length; i++) {
       var row = [];
       for (var j = 0; j < this.map[i].length; j++) {
-        var color = DEFAULT_COLORS[this.map[i][j]];
         if (this.map[i][j] === EARTH_POINT_TYPE) {
-          color = this.generateRandomColor();
+          var island = islands[[i, j]];
+          row.push(island.color);
+        } else {
+          row.push(DEFAULT_COLORS[WATER_POINT_TYPE]);
         }
-        row.push(color);
       }
       coloredMap.push(row);
     }
     return coloredMap;
+  }
+
+  getIslands() {
+    var islands = {};
+    for (var i = 0; i < this.map.length; i++) {
+      for (var j = 0; j < this.map[i].length; j++) {
+        if (this.map[i][j] === EARTH_POINT_TYPE) {
+          var island = undefined;
+          var neighborColors = [];
+          if (i > 0 && this.map[i - 1][j] === EARTH_POINT_TYPE) {
+            var leftEarth = islands[[i - 1, j]];
+            if (leftEarth !== undefined) {
+              island = leftEarth;
+              neighborColors.push(leftEarth.color);
+            }
+          }
+          if (j > 0 && this.map[i][j - 1] === EARTH_POINT_TYPE) {
+            var earthAbove = islands[[i, j - 1]];
+
+            if (earthAbove !== undefined) {
+              island = earthAbove;
+              neighborColors.push(earthAbove.color);
+            }
+          }
+          if (island === undefined) {
+            var color = this.generateRandomColor();
+            island = { color: color };
+          }
+          islands[[i, j]] = island;
+          var color = island.color;
+          island.color = color;
+        }
+      }
+    }
+    return islands;
   }
 }
